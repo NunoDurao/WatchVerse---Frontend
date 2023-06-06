f2323bb1788f4fb3adf2153cb4910d046a715f17
Francisco Branco <franciscomcb782@gmail.com>
2023-06-06 17:14:52 +0100

    movie cards

--- src/Pages/MovieList.jsx
+++ src/Pages/MovieList.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import AddMovie from "./AddMovies";
+import Grid from "@mui/material/Grid";
 
 const API_URL = "http://localhost:5005";
 
@@ -42,16 +46,33 @@ function MoviesListPage() {
         <button onClick={getAllMovies}>Filter</button>
       </div>
       <AddMovie refreshMovies={getAllMovies} />
-      {movies.map((movie) => {
-        return (
-          <div className="movie-card card" key={movie._id}>
-            <Link to={`/movies/${movie._id}`}>
-              <h3>{movie.title}</h3>
-              <img className="list-image" src={movie.image} alt={movie.title} />
-            </Link>
-          </div>
-        );
-      })}
+      <Grid container spacing={2}>
+        {movies.map((movie) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={movie._id}>
+            <Card sx={{ height: "100%" }}>
+              <Link to={`/movies/${movie._id}`}>
+                <CardMedia
+                  component="img"
+                  src={movie.image}
+                  alt={movie.title}
+                  style={{
+                    objectFit: "contain",
+                    maxHeight: "300px", // Adjust the maxHeight as per your preference
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {movie.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {movie.description}
+                  </Typography>
+                </CardContent>
+              </Link>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }

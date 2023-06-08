0a2d4fcdf1dcf9630df3a3467612a00a37fcf8bd
Francisco Branco <franciscomcb782@gmail.com>
2023-06-08 12:26:37 +0100

    changes

--- src/Pages/SeriesList.jsx
+++ src/Pages/SeriesList.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import AddSeries from "./AddSeries";
 import Grid from "@mui/material/Grid";
+import Pagination from "@mui/material/Pagination";
 
 const API_URL = import.meta.env.VITE_APP_SERVER_URL;
+const PAGE_SIZE = 12; // Number of series per page
 
 function SeriesListPage() {
   const [series, setSeries] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const getAllSeries = () => {
     axios
@@ -24,36 +23,49 @@ function SeriesListPage() {
     getAllSeries();
   }, []);
 
+  // Calculate total number of pages based on series count and page size
+  const totalPages = Math.ceil(series.length / PAGE_SIZE);
+
+  // Get the current page series based on the currentPage state
+  const currentSeries = series.slice(
+    (currentPage - 1) * PAGE_SIZE,
+    currentPage * PAGE_SIZE
+  );
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="series-list-page">
-      <AddSeries refreshSeries={getAllSeries} />
       <Grid container spacing={2}>
-        {series.map((serie) => (
+        {currentSeries.map((serie) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={serie._id}>
-            <Card sx={{ height: "100%" }}>
-              <Link to={`/series/${serie._id}`}>
+            <Link to={`/series/${serie._id}`}>
+              <div style={{ position: "relative", height: "100%" }}>
                 <CardMedia
                   component="img"
                   src={serie.image}
                   alt={serie.title}
                   style={{
-                    objectFit: "contain",
-                    maxHeight: "300px", // Adjust the maxHeight as per your preference
+                    height: "100%",
+                    objectFit: "cover",
+                    objectPosition: "center",
                   }}
                 />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {serie.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {/* Add other series details here */}
-                  </Typography>
-                </CardContent>
-              </Link>
-            </Card>
+              </div>
+            </Link>
           </Grid>
         ))}
       </Grid>
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={handlePageChange}
+        siblingCount={1}
+        boundaryCount={1}
+        color="primary"
+      />
     </div>
   );
 }

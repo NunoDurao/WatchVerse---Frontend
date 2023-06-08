52424e79ca0aba2db8ca868879877470ff0d7e62
Nuno Durão <nunofdurao@gmail.com>
2023-06-08 15:24:58 +0100

    homepage changes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
       <div className="text-white homepage-content center-content">
         <div className="right-aligned-content">
           <h1 className="white-text">WatchVerse</h1>
-          <h2 className="white-text">Track. Discover. Watch. WatchVerse.</h2>
+          <h2 className="white-text">WatchVerse and chill... with a random thrill!</h2>
         </div>
         <div className="right-aligned-buttons">
           <Link to="/signup" style={{ textDecoration: "none" }}>
@@ -20,16 +20,6 @@ function HomePage() {
           <Link to="/login" style={{ textDecoration: "none" }}>
             <button className="btn btn-outline-primary btn-lg m-2">
               Login
-            </button>
-          </Link>
-          <Link to="/movies" style={{ textDecoration: "none" }}>
-            <button className="btn btn-outline-primary btn-lg m-2">
-              Movies List
-            </button>
-          </Link>
-          <Link to="/series" style={{ textDecoration: "none" }}>
-            <button className="btn btn-outline-primary btn-lg m-2">
-              Series List
             </button>
           </Link>
         </div>

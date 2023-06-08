8bbbeb84ad50293c2de345a4c3afa6828f43c203
Nuno Durão <nunofdurao@gmail.com>
2023-06-08 11:55:50 +0100

    css

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import * as React from "react";
 import { useState, useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";

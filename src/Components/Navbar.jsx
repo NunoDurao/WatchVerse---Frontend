e39e0357494e79409a51601d1ab5721e95342cba
Francisco Branco <franciscomcb782@gmail.com>
2023-06-08 17:05:34 +0100

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
         <Typography
           variant="h6"
           component="div"
-          sx={{ flexGrow: 1, marginLeft: "50px" }}
+          sx={{ flexGrow: 1, marginLeft: "100px" }}
         >
           WatchVerse
         </Typography>
